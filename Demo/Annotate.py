3ee1ca4d97f536af9ce10916c3dc18b2deda6352
SDYL <1593914190@qq.com>
2019-07-13 15:53:20 +0800

    Data Update
    Signed-off-by: SDYL <1593914190@qq.com>

--- Demo/Annotate.py
+++ Demo/Annotate.py
@@ -4,7 +4,7 @@ import numpy as np
 x = np.linspace(0.05, 10, 1000)
 y = np.sin(x)
 
-plt.plot(x, y, ls="-.", lw=2, c="c",label="plot figure")
+plt.plot(x, y, ls="-.", lw=2, c="c", label="plot figure")
 
 plt.legend()
 plt.annotate("maximum",
--- Statistical graph/accumulation.py
+++ Statistical graph/accumulation.py
@@ -3,6 +3,7 @@
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
 
 mpl.rcParams["font.sans-serif"] = ["SimHei"]
 # 完成字体配置任务
@@ -10,12 +11,19 @@ mpl.rcParams["axes.unicode_minus"] = False
 
 # some simple data
 
-x = [1, 2, 3, 4, 5]
+x = np.arange(5)
 y = [6, 10, 4, 5, 1]
+y1 = [2, 6, 3,  8, 5]
+
+bar_width = 0.35
+tick_label = ["A", "B", "C", "D", "E"]
 
 # creat bar
-plt.bar(x, y, align="center", color="b",
-        tick_label=["A", "B", "C", "D", "E"],
+plt.bar(x, y, bar_width, align="center", color="c",
+        label="班级A",
+        alpha=0.5)
+plt.bar(x+bar_width, y1, bar_width, align="center", color="b",
+        label=" 班 级B",
         alpha=0.5)
 # x 柱状图中柱体的标签值
 # y 柱体高度
@@ -28,6 +36,7 @@ plt.ylabel("试卷份数")
 
 # set yaxis grid
 
-plt.grid(True, axis="y", ls=":", color="r", alpha=0.3)
-
+# plt.grid(True, axis="y", ls=":", color="r", alpha=0.3)
+plt.xticks(x + bar_width / 2, tick_label)
+plt.legend()
 plt.show()
